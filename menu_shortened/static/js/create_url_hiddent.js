2c5dbab5a85ef79dfb5667b9080bbadf043735d0
Prod <pyrod7@gmail.com>
2024-03-17 11:18:08 -0600

    Se creo el script especifico para el texto

--- menu_shortened/static/js/create_url_hiddent.js
+++ menu_shortened/static/js/create_url_hiddent.js
@@ -1,2 +1,13 @@
-let para = document.getElementsByClassName('.text-enter-create')
-console.log(para)
+document.addEventListener("DOMContentLoaded", function () {
+    let text = document.querySelector('.text-enter-create');
+    text.style.display = 'none'
+    let inputOrigin = document.getElementById('id_original_url');
+
+    inputOrigin.addEventListener('input', function (event) {
+        if (event.target.value !== '') {
+            text.style.display = 'block';
+        } else {
+            text.style.display = 'none';
+        }
+    });
+});
