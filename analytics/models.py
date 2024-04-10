ec7e6be33e6b559b0462c395a536545942f5b2e3
Prod <pyrod7@gmail.com>
2024-04-09 20:22:09 -0600

    Manejo de las analiticas

---
+++ analytics/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class UrlAnalytics(models.Model):
+    id_short_url = models.CharField(
+        max_length=500, default='', unique=False, blank=False)
+    is_mobile = models.BooleanField(default=False)
+    is_tablet = models.BooleanField(default=False)
+    is_pc = models.BooleanField(default=False)
+    is_touch_capable = models.BooleanField(default=False)
+    is_bot = models.BooleanField(default=False)
+    browser = models.CharField(max_length=200, default='', blank=True)
+    mobile_system = models.CharField(max_length=100, default='', blank=True)
+    device = models.CharField(max_length=100, default='', blank=True)
+    created = models.DateTimeField(auto_created=True, auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Analytics'
+        db_table = 'Analytics'
+
+    def __str__(self) -> str:
+        return f'Analytic data has been created to {self.id_short_url}'
