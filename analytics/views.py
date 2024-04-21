d8778c7508441f4373397c6cd7992ec8c3260fdd
Prod <pyrod7@gmail.com>
2024-04-21 11:16:51 -0600

    Modificacion del template

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from url_link.models import SaveUrlShortened
+from qr_codes_link.models import QRGenerator
 
 # Create your views here.
+
+
+def analytics(request):
+    popular_click = SaveUrlShortened.objects.all().order_by('-clicks').first()
+    popular_qr_code = QRGenerator.objects.all().order_by('-scans').first()
+
+    return render(request, 'analytics.html', {
+        'click': popular_click,
+        'scan': popular_qr_code
+    })
