73de88e7e64d0d18ad855874d167d369d6ff8f97
pyrod <pyrod7@gmail.com>
2024-04-25 18:41:49 -0600

    Agregando estilos

--- analytics/views.py
+++ analytics/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from url_link.models import SaveUrlShortened
 from qr_codes_link.models import QRGenerator
+from django.contrib.sites.shortcuts import get_current_site
 
 # Create your views here.
 
@@ -8,8 +9,9 @@ from qr_codes_link.models import QRGenerator
 def analytics(request):
     popular_click = SaveUrlShortened.objects.all().order_by('-clicks').first()
     popular_qr_code = QRGenerator.objects.all().order_by('-scans').first()
-
+    get_domain = get_current_site(request).domain
     return render(request, 'analytics.html', {
         'click': popular_click,
-        'scan': popular_qr_code
+        'scan': popular_qr_code,
+        'domain': get_domain,
     })
